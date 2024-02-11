34e8718b32f4f93662a76f2a858fbe080dab8ec7
ADITI <aditigaikwad0110.com>
2024-02-11 16:06:52 +0530

    react-techrock

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <h1>TechRock</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/abc" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/abc" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/abc" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
